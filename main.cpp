df78cbff416a91b80ce6f2d1cf989172185bd394
razar-dev <razar.dev@yandex.by>
2022-01-30 16:34:29 +0300

    first version

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
-#include <iostream>
+#include <Windows.h>
+
+HWND find_window() {
+    HWND hwnd_host = nullptr;
+    HWND hwnd_ret = nullptr;
+
+    int count = 0;
+
+    while ((hwnd_host = FindWindowEx(nullptr, hwnd_host, "NativeHWNDHost", "")) != nullptr) {
+        if (FindWindowEx(hwnd_host, nullptr, "DirectUIHWND", "") != nullptr) {
+            if (count == 0) {
+                hwnd_ret = hwnd_host;
+            }
+
+            count++;
+
+            if (count > 1)
+                return nullptr;
+
+        }
+    }
+    return hwnd_ret;
+}
 
 int main() {
+    HWND volume_window = find_window();
+
+    WINDOWPLACEMENT wd;
+    GetWindowPlacement(volume_window, &wd);
+
+    if (wd.showCmd == SW_SHOWMINIMIZED)
+        ShowWindow(volume_window, SW_RESTORE);
+    else
+        ShowWindow(volume_window, SW_MINIMIZE);
+
     return 0;
 }
